1dac199c4e056afbee16043b735dbefe35352d2e
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-10-28 17:59:18 +0700

    add cancel order button

--- Orderin/Features/MainPage/Home/HomeViewController.swift
+++ Orderin/Features/MainPage/Home/HomeViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        cancelOrder()
     }
-
-
+    
+    // Cancel Orde
+    func cancelOrder() {
+        
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 250, height: 50)
+        button.backgroundColor = UIColor.red
+        button.setTitle("Cancel Order Button ", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+        
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
+    }
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Orderin/ViewController.swift
+++ Orderin/ViewController.swift
@@ -14,21 +14,5 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
-    
-    func cancelOrder() {
-        
-        let button = UIButton()
-         button.frame = CGRect(x: self.view.frame.size.width - 60, y: 60, width: 50, height: 50)
-         button.backgroundColor = UIColor.red
-         button.setTitle("Name your Button ", for: .normal)
-         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-         self.view.addSubview(button)
-
-   
-    }
-    
-    @objc func buttonAction(sender: UIButton!) {
-       print("Button tapped")
-    }
+  
 }
