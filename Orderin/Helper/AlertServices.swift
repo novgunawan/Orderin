a8525229ee1d47bbec6d43e0b94091a2b216d901
novgunawan <novgunawan07@gmail.com>
2021-11-02 23:39:05 +0700

    feat: Add navigations from sign in pop up to sign in page

--- Orderin/Helper/AlertServices.swift
+++ Orderin/Helper/AlertServices.swift
@@ -10,31 +10,29 @@ import UIKit
 class AlertServices {
     
     static func presentAlertSignedIn(onVC: UIViewController, message: String) {
+        
         let signinAlertVC = UIAlertController(title: "Sign in to continue", message: message, preferredStyle: .alert)
+        
         // MARK: Signed In Action Handler
         let signinAction = UIAlertAction(title: "Sign In", style: .default) { _ in
             let signinVC = SignInViewController()
-//            onVC.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
             signinVC.modalPresentationStyle = .fullScreen
-            onVC.present(signinVC, animated: true, completion: nil)
-
-//            onVC.navigationController?.pushViewController(SignInViewController(), animated: true)
-//            onVC.modalTransitionStyle = .crossDissolve
-//            onVC.modalPresentationStyle = .fullScreen
-            
-
-//            onVC.view.window?.rootViewController = SignInViewController()
+            DispatchQueue.main.async {
+                onVC.present(signinVC, animated: true, completion: nil)
+            }
         }
         
-        // MARK: Not Now Action Handler
+        // MARK: Cancel Action Handler
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
             signinAlertVC.dismiss(animated: true, completion: nil)
         }
         
+        // MARK: Add actions to alert VC
         let actions = [signinAction, cancelAction]
         for action in actions {
             signinAlertVC.addAction(action)
         }
+        
         onVC.present(signinAlertVC, animated: true, completion: nil)
     }
 }
