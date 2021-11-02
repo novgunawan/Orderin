b949dbc99f94bb18bc4b51d6fe42b46ef63e85a4
novgunawan <novgunawan07@gmail.com>
2021-11-02 23:44:22 +0700

    feat: fix user's state on signing in

--- Orderin/AppDelegate.swift
+++ Orderin/AppDelegate.swift
@@ -11,7 +11,7 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
@@ -22,39 +22,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let user = Auth.auth().currentUser
         var credential: AuthCredential
         
-        user?.delete { error in
-            if let error = error {
-                // An error happened.
+        Auth.auth().addStateDidChangeListener({ auth, user in
+            if let user = user {
+                // MARK: User is signed in.
+                self.window?.rootViewController = home
+                
             } else {
-                // Account deleted.
-                print("acount deleted")
+                // MARK: User is not signed in.
+                let signInVC = SignInViewController()
+                self.window?.rootViewController = signInVC
             }
-        }
-                Auth.auth().addStateDidChangeListener({ auth, user in
-                    if let user = user {
-                        // MARK: User is signed in.
-                        self.window?.rootViewController = home
-        
-                    } else {
-                        // MARK: User is not signed in.
-                        let signInVC = SignInViewController()
-                        self.window?.rootViewController = signInVC
-                    }
-                })
+        })
         window?.makeKeyAndVisible()
         return true
     }
- 
+    
     func applicationWillTerminate(_ application: UIApplication) {
         let user = Auth.auth().currentUser
-        user?.delete { error in
-          if let error = error {
-            // An error happened.
-          } else {
-            // Account deleted.
-              print("acount deleted")
-          }
-        }
+        //        user?.delete { error in
+        //          if let error = error {
+        //            // An error happened.
+        //          } else {
+        //            // Account deleted.
+        //              print("acount deleted")
+        //          }
+        //        }
     }
     // MARK: UISceneSession Lifecycle
     
