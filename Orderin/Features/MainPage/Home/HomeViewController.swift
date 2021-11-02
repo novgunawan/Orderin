c1ebf870871c9bf2aca4eae08601d0203b4563cd
novgunawan <novgunawan07@gmail.com>
2021-11-02 23:36:23 +0700

    refactor: Give comments for the code feat: Set tabbar's item color

--- Orderin/Features/MainPage/Home/HomeViewController.swift
+++ Orderin/Features/MainPage/Home/HomeViewController.swift
@@ -12,7 +12,11 @@ import Firebase
 class HomeViewController: UIViewController {
     
     static var signinViewController = SignInViewController()
+    var tabBarTag: Bool = true
     
+    // MARK: -Properties
+    
+    // MARK: Scan QR Button
     lazy var scanQRButton: UIButton = {
         let button = UIButton()
         button.setTitle("Scan QR Code", for: .normal)
@@ -24,13 +28,57 @@ class HomeViewController: UIViewController {
         
         return button
     }()
+    
+    // MARK: Sign Out Button
+    let signoutButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(C.signoutButton, for: .normal)
+        button.titleLabel?.font = UIFont(name: C.fontPoppinsRegular, size: C.fontsizeBody)
+        button.setTitleColor(C.hexStringToUIColor(hex: C.red50), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(signout), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: -App Lifecycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // MARK: Set selected tab bar item's color
+        if tabBarTag == true {
+            self.tabBarController?.tabBar.tintColor = C.hexStringToUIColor(hex: C.teal50)
+        } else {
+            self.tabBarController?.tabBar.tintColor = C.hexStringToUIColor(hex: C.gray70)
+        }
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scanQRButton.translatesAutoresizingMaskIntoConstraints = false
+        scanQRButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        scanQRButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0).isActive = true
+        scanQRButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        scanQRButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+        
+        // MARK: Constraint Sign Out Button
+        
+        signoutButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 72.0).isActive = true
+        signoutButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10.0).isActive = true
+        signoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 10.0).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(scanQRButton)
+        view.addSubview(signoutButton)
         cancelOrder()
         view.backgroundColor = .white
         
     }
+    
+    // MARK: -Functions
+    
     @objc func scanQR() {
         // MARK: Check user has signed in or not
         
@@ -46,13 +94,31 @@ class HomeViewController: UIViewController {
         })
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        scanQRButton.translatesAutoresizingMaskIntoConstraints = false
-        scanQRButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        scanQRButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0).isActive = true
-        scanQRButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        scanQRButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+    @objc func signout() {
+        Auth.auth().addStateDidChangeListener({ auth, user in
+            if let user = user {
+                // MARK: User is signed in.
+                
+                let firebaseAuth = Auth.auth()
+                let user = Auth.auth().currentUser
+                do {
+                    try firebaseAuth.signOut()
+                } catch let signOutError as NSError {
+                    print("Error signing out: %@", signOutError)
+                }
+                user?.delete { error in
+                    if let error = error {
+                        // An error happened.
+                    } else {
+                        // Account deleted.
+                        print("acount deleted")
+                    }
+                }
+            } else {
+                // MARK: User is not signed in.
+                print("from home view controller : you haven't even signed in")
+            }
+        })
     }
     
     @IBAction func menuDetailDidtTapped(_ sender: Any) {
@@ -60,7 +126,8 @@ class HomeViewController: UIViewController {
         vc.modalPresentationStyle = .formSheet
         self.present(vc, animated: true, completion: nil)
     }
-    // Cancel Orde
+    
+    // MARK: Cancel Order
     func cancelOrder() {
         
         let button = UIButton()
@@ -80,15 +147,6 @@ class HomeViewController: UIViewController {
         self.present(cancelViewController, animated: true, completion: nil)
         
     }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
 
