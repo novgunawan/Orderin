38542d3941a8d0ab86ec9af325023eb574ac7dd5
Nathaniel Andrian <nathaniel.taskl@gmail.com>
2021-11-10 23:19:14 +0700

    feat: search functionality, bug: cancel in search button make table view index out of range

--- Orderin/Features/MenuList/ViewModel/MenuListViewModel.swift
+++ Orderin/Features/MenuList/ViewModel/MenuListViewModel.swift
@@ -10,7 +10,8 @@ import UIKit
 
 struct MenuListViewModel {
     
-    var menuData : [Menu] = []
+    var menuData: [Menu] = []
+    var menuWithoutCategory: [MenuListModel] = []
     
     
     // Fetching data
@@ -31,6 +32,46 @@ struct MenuListViewModel {
         
         menuData = [data1,data2,data3,data4]
         completion(self.menuData)
+        
+        
+    }
+    
+    mutating func fetchMenuWithoutCategory(completion: @escaping ([MenuListModel])->Void){
+        
+        let data1 = MenuListModel(menuID:"XYZ123",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000",availability: true)
+        let data2 = MenuListModel(menuID:"XYZ456",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability: true)
+        
+        let data3 = MenuListModel(menuID: "XYZ456",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability: true)
+        
+        let data4 = MenuListModel(menuID:"XYZ746",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability: true)
+        
+        let data5 = MenuListModel(menuID:"ABC123",image: UIImage(named: "food-dummy")!, title: "Nasi Goreng", description: "ini nasi goreng enak banget", price: "Rp30.000",availability: true)
+        
+        let data6 = MenuListModel(menuID:"ABC353",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000",availability: true)
+        
+        let data7 = MenuListModel(menuID: "ABC389",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000",availability: true)
+        
+        let data8 = MenuListModel(menuID: "ABC837",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability: true)
+        
+        let data9 = MenuListModel(menuID:"DEF345",image: UIImage(named: "food-dummy")!, title: "Puding", description: "ini puding enak banget", price: "Rp45.000", availability: true)
+        
+        let data10 = MenuListModel(menuID:"DEF849",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability:true)
+            
+        let data11 = MenuListModel(menuID: "DEF859",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability:  true)
+        
+        let data12 = MenuListModel(menuID:"DEF849",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability: true)
+        
+        let data13 = MenuListModel(menuID: "JKL849",image: UIImage(named: "food-dummy")!, title: "Jus Jeruk", description: "ini jus jeruk enak banget", price: "Rp25.000", availability: true)
+        
+        let data14 = MenuListModel(menuID:"JKL984",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability:  true)
+        
+        let data15 = MenuListModel(menuID: "JKL909",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability: true)
+        
+        let data16 = MenuListModel(menuID:"JKl80",image: UIImage(named: "food-dummy")!, title: "Burger", description: "ini burger enak banget", price: "Rp35.000", availability: true)
+        
+        
+        menuWithoutCategory = [data1,data2,data3,data4,data5,data6,data7,data8,data9,data10,data11,data12,data13,data14,data15,data16]
+        completion(self.menuWithoutCategory)
         
         
     }
