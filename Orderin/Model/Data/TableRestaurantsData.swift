89387f7b3192608c816fc7d4a68d1b52e8b0e2f6
novgunawan <novgunawan07@gmail.com>
2021-11-10 23:30:10 +0700

    feat: Create data customization [not done] feat: Create Table Restaurant Model

---
+++ Orderin/Model/Data/TableRestaurantsData.swift
@@ -0,0 +1,16 @@
+//
+//  TableRestaurantsData.swift
+//  Orderin
+//
+//  Created by Novi Gunawan on 10/11/21.
+//
+
+import Foundation
+
+var tableRestaurants = [
+    TableRestaurants(tableNumber: 1, availability: true),
+    TableRestaurants(tableNumber: 2, availability: true),
+    TableRestaurants(tableNumber: 3, availability: false),
+    TableRestaurants(tableNumber: 4, availability: true),
+    TableRestaurants(tableNumber: 5, availability: false),
+]
---
+++ Orderin/Model/TableRestaurants.swift
@@ -0,0 +1,14 @@
+//
+//  TableRestaurants.swift
+//  Orderin
+//
+//  Created by Novi Gunawan on 10/11/21.
+//
+
+import Foundation
+
+struct TableRestaurants {
+    let tableNumber: Int
+    let availability: Bool
+//    let orders: [Order]
+}
