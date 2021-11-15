2c4ada26fbcdc325951802deb53854e5f29943f0
pieteryonathan <pieteryonathan20@gmail.com>
2021-11-15 14:54:42 +0700

    Feat: tambah todo

--- Orderin/Features/OrderSummary/ViewModel/ExtensionTableViewOrderSummary.swift
+++ Orderin/Features/OrderSummary/ViewModel/ExtensionTableViewOrderSummary.swift
@@ -12,6 +12,9 @@ extension OrderSummaryViewController: UITableViewDelegate, UITableViewDataSource
     
     //MARK: - Setup Cell in Table View
     
+    //TO-DO: Logic for number of row
+    //TO-DO: get data in database for number of row
+    
     //Height Cell
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row < 1{
