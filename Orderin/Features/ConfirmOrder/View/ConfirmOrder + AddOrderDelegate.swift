b60828b885a9f10bbc50173cdb453284ec62b7aa
pieteryonathan <pieteryonathan20@gmail.com>
2021-11-24 17:05:32 +0700

    bug: mentok bunnd

--- Orderin/Features/ConfirmOrder/View/ConfirmOrder + AddOrderDelegate.swift
+++ Orderin/Features/ConfirmOrder/View/ConfirmOrder + AddOrderDelegate.swift
@@ -10,22 +10,17 @@ import Foundation
 extension ConfirmOrderViewController: addNewMenuDelegate {
     
     func addOrder() {
-//        navigationController?.popViewController(animated: true)
-                let controllers = self.navigationController?.viewControllers
-                              for vc in controllers! {
-                                if vc is MenuListViewController {
-                                  _ = self.navigationController?.popToViewController(vc as! MenuListViewController, animated: true)
-                                }
-                             }
+        navigationController?.popViewController(animated: true)
+//        let controllers = self.navigationController?.viewControllers
+//                for vc in controllers! {
+//                    if vc is MenuListViewController {
+//                        _ = self.navigationController?.popToViewController(vc as! MenuListViewController, animated: true)
+//                                }
+//                             }
+//        self.dismiss(animated: true, completion: nil)
     }
     
     func dropButtonNav(){
-//        let controllers = self.navigationController?.viewControllers
-//                      for vc in controllers! {
-//                        if vc is HomeViewController {
-//                          _ = self.navigationController?.popToViewController(vc as! HomeViewController, animated: true)
-//                        }
-//                     }
         navigationController?.popViewController(animated: true)
         navigationController?.popViewController(animated: true)
     }
